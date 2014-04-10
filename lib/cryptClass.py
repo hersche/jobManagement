931e18bd8ac0621f66ba6b5dac869df612381fd8
skamster <hersche@vmail.me>
2014-04-10 20:38:26 +0200

    merge cryptClass with the tryToxic-one

--- lib/cryptClass.py
+++ lib/cryptClass.py
@@ -18,32 +18,37 @@ class cm:
         self.rand = rand
 
     def setKey(self, key):
+      """
+      self.setKey does hash the password and shortens the hash to max of algorithm's key-block_size.
+      return new Key
+      """
+      if len(key) is not 0:
         hash = SHA512.new(bytes(key,'ascii'))
         self.key = hash.hexdigest()
         if self.name == "CAST" or self.name=="IDEA":
-           self.key = self.key[0:16]
-        elif self.name == "Blowfish" or self.name == "ARC2" or self.name == "ARC4":
-           if len(self.key) < self.mod.block_size:
-             rest = (self.mod.block_size *2) - len(self.key)
-           else:
-             rest = 16
-           while rest !=0:
-             rest -=1
-             self.key += "."
+          self.key = self.key[0:16]
+        elif self.name == "ARC2":
+          self.key = self.key[0:127]
+        elif self.name == "Blowfish"  or self.name == "ARC4":
+          self.key = self.key[0:56]
         elif self.name == "AES" or self.name == "XOR":
-           self.key = self.key[0:32]
+          self.key = self.key[0:32]
         elif self.name == "DES3":
           self.key = self.key[0:24]
-           
-           
-        #logger.error(self.key + " len "+str(len(self.key)))
         return self.key
+      else:
+        self.mod = None
+        self.name = "None"
 
     def encrypt(self, rawMessage):
+      """
+      Encrypt plaintext.
+      return encryptedMessage
+      """
       try:
         message=str(rawMessage)
-        if self.mod == None:
-            return rawMessage
+        if self.mod == None and self.name=="None" and self.key != "encryptionInit":
+            return message
         iv = self.rand.new().read(self.mod.block_size)
         if self.name == "XOR" or self.name == "ARC4":
           cipher = self.mod.new(self.key)          
@@ -57,34 +62,39 @@ class cm:
         tmp = ""
         while rest != 0:
             rest -=1
-            tmp += " "
+            tmp += "."
         eMessage = message+tmp
-        #print(eMessage+str(len(eMessage)))
         t =  base64.b64encode(iv + cipher.encrypt(eMessage))
         return t
       except Exception as e:
-        logger.error("|Crypt| Encryptionerror: "+ str(e.args[0]))
+        logger.error(tr("|Crypt| Encryptionerror: ")+ str(e.args[0])+tr(" Message ")+str(rawMessage))
     def decrypt(self, encryptedMessage):
+        """
+        Decrypt encrypted text.
+        return clearText
+        """
         try:
-          if self.mod == None:
-              return encryptedMessage
           if encryptedMessage is None:
               return ""
-          tDec = base64.b64decode(encryptedMessage)
-          iv = tDec[:self.mod.block_size]
-          if self.name == "XOR" or self.name == "ARC4":
-            cipher = self.mod.new(self.key)          
+          if self.mod != None and self.name!="None" and self.key != "encryptionInit":
+            tDec = base64.b64decode(encryptedMessage)
+            iv = tDec[:self.mod.block_size]
+            if self.name == "XOR" or self.name == "ARC4":
+              cipher = self.mod.new(self.key)          
+            else:
+              cipher = self.mod.new(self.key, self.mod.MODE_CBC, iv)
+              
+            clearText = str(cipher.decrypt(tDec[self.mod.block_size:]))
+            #workAround for b'-signed floats and ints..
+            if clearText[0:2] == "b'":
+                clearText = clearText[2:-1]
+            else:
+                clearText = clearText[0:-1]
+            return clearText.rstrip(".")
           else:
-            cipher = self.mod.new(self.key, self.mod.MODE_CBC, iv)
-          clearText = str(cipher.decrypt(tDec[self.mod.block_size:]))
-          #workAround for b'-signed floats and ints..
-          if clearText[0:2] == "b'":
-              clearText = clearText[2:-1]
-          else:
-              clearText = clearText[0:-1]
-          return clearText.rstrip()
+            return encryptedMessage
         except Exception as e:
-          logger.error("|Crypt| Decryptionerror: "+ str(e.args[0]))
+          logger.error(tr("|Crypt| Decryptionerror: ")+ str(e.args[0]))
 
 #static crypt manager
 class scm:
@@ -109,39 +119,44 @@ class scm:
       for pf in controller.personalFinances:
           pf.save(pf.name, pf.value, pf.date, pf.repeat, pf.timesRepeat, pf.plusMinus, pf.active)
 
-    @staticmethod
-    def getMod(configValue):
-        configValue = configValue.lower()
-        try:
-            if configValue == "1" or configValue == "cast":
-                if("CAST" not in sys.modules):
-                    from Crypto.Cipher import CAST
-                return CAST
-            elif configValue == "2"  or configValue == "blowfish":
-                if("Blowfish" not in sys.modules):
-                    from Crypto.Cipher import Blowfish
-                return Blowfish
-            elif configValue == "3"  or configValue == "des3":
-                if("DES3" not in sys.modules):
-                    from Crypto.Cipher import DES3
-                return DES3
-            elif configValue == "4"  or configValue == "arc4":
-                if("ARC4" not in sys.modules):
-                    from Crypto.Cipher import ARC4
-                return ARC4
-            elif configValue == "5"  or configValue == "xor":
-                if("XOR" not in sys.modules):
-                    from Crypto.Cipher import XOR
-                return XOR
-            elif configValue == "6"  or configValue == "aes":
-                if("AES" not in sys.modules):
-                    from Crypto.Cipher import AES
-                return AES
-            elif configValue == "7"  or configValue == "arc2":
-                if("ARC2" not in sys.modules):
-                    from Crypto.Cipher import ARC2
-                return ARC2
-            else:
-                return None
-        except  Exception as e:
-            logger.error("Could not find pycrypt-Module. Please install it via apt-get install python3-crypto")
+  @staticmethod
+  def getMod(configValue):
+    """
+    get cryptomodule for cm. This is specialised for values, which you should enter in config.
+    return cryptoModule or None in case of not found.
+    """
+    configValue = configValue.lower()
+    try:
+        if configValue == "1" or configValue == "cast":
+            if("CAST" not in sys.modules):
+                from Crypto.Cipher import CAST
+            return CAST
+        elif configValue == "2"  or configValue == "blowfish":
+            if("Blowfish" not in sys.modules):
+                from Crypto.Cipher import Blowfish
+            return Blowfish
+        elif configValue == "3"  or configValue == "des3":
+            if("DES3" not in sys.modules):
+                from Crypto.Cipher import DES3
+            return DES3
+        elif configValue == "4"  or configValue == "arc4":
+            if("ARC4" not in sys.modules):
+                from Crypto.Cipher import ARC4
+            return ARC4
+        elif configValue == "5"  or configValue == "xor":
+            if("XOR" not in sys.modules):
+                from Crypto.Cipher import XOR
+            return XOR
+        elif configValue == "6"  or configValue == "aes":
+            if("AES" not in sys.modules):
+                from Crypto.Cipher import AES
+            return AES
+        elif configValue == "7"  or configValue == "arc2":
+            if("ARC2" not in sys.modules):
+                from Crypto.Cipher import ARC2
+            return ARC2
+        else:
+            return None
+    except  Exception as e:
+        logger.error(tr("Could not find pycrypt-Module. Please install it via apt-get install python3-crypto"))
+        return None
